4f28c8a8eff5ac2e395bf845bc35e2d08bd40da1
haoxiangpeng <3150102255@zju.edu.cn>
2017-10-10 21:13:39 -0700

    adjust css style

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,20 +1,27 @@
 <template>
   <div style="width:100%;height:100%;position:absolute;">
     <div style="padding:2em;">
-      <div>Large dataset may slow down your computer, Chrome is recommended.</div>
-      <div id='nhldata' style="min-width:400px;min-height:400px;">
+      <div style="margin:0 auto 2em auto;">
+        <div style="display:flex;justify-content:space-evenly;">
+          <div style="display:flex;justify-content:center;flex-direction:column;">Control Panel: </div>
+          <el-select @change="updateGraph" v-model="selectedColumn" placeholder="select predictor">
+            <el-option v-for="(item,index) in labels" :key="index" :label="item" :value="item">
+            </el-option>
+          </el-select>
+          <el-select @change="updateGraph" v-model="leafNode" placeholder="select leaf node">
+            <el-option label="all" :value="-1">
+            </el-option>
+            <el-option v-for="(item,index) in allLeaf" :key="index" :label="item" :value="item">
+            </el-option>
+          </el-select>
+          <el-date-picker v-model="startYear" type="year" @change="updateGraph" placeholder="Starting year">
+          </el-date-picker>
+          <el-date-picker v-model="endYear" type="year" @change="updateGraph" placeholder="Ending year">
+          </el-date-picker>
+          <el-button @click="updateGraph">Refresh</el-button>
+        </div>
       </div>
-      <div>
-        <div>Control Panel: </div>
-        <el-select @change="updateGraph" v-model="selectedColumn" placeholder="select predictor">
-          <el-option v-for="(item,index) in labels" :key="index" :label="item" :value="item">
-          </el-option>
-        </el-select>
-        <el-date-picker v-model="startYear" type="year" @change="updateGraph" placeholder="Starting year">
-        </el-date-picker>
-        <el-date-picker v-model="endYear" type="year" @change="updateGraph" placeholder="Ending year">
-        </el-date-picker>
-        <el-button @click="updateGraph">Refresh</el-button>
+      <div id='nhldata' style="min-width:400px;min-height:400px;">
       </div>
     </div>
   </div>
@@ -22,10 +29,11 @@
 
 <script>
 import echarts from 'echarts'
-import playerRawData from '../assets/joinData.csv'
-const INDEX_Y = 22
-const INDEX_YEAR = 8
+import playerRawData from '../assets/joined_leaf_node.csv'
+const INDEX_Y = 11
+const INDEX_YEAR = 5
 const INDEX_NAME = 1
+const INDEX_LEAF = 10
 export default {
   name: 'template',
   data() {
@@ -36,20 +44,27 @@ export default {
       plotData: [],
       selectedColumn: 'Weight',
       startYear: new Date(2004, 1, 1),
-      endYear: new Date(2008, 1, 1)
+      endYear: new Date(2008, 1, 1),
+      leafNode: -1,
+      allLeaf: [1, 2, 3, 4, 5, 6, 7]
     }
   },
   mounted() {
     this.myChart = echarts.init(document.getElementById('nhldata'))
     this.labels = playerRawData[0]
     this.allData = playerRawData.slice(1)
+    this.$message('Large dataset may slow down your computer, Chrome is recommended.')
   },
   methods: {
     drawPlot: function(plotData) {
       let self = this
       this.myChart.setOption({
-        title: { text: 'sum_7yr_GP vs ' + self.selectedColumn },
+        title: {
+          text: 'sum_7yr_GP vs ' + self.selectedColumn,
+          left: 'center'
+        },
         xAxis: {
+          name: self.selectedColumn,
           splitLine: {
             lineStyle: {
               type: 'dashed'
@@ -58,6 +73,7 @@ export default {
           scale: true
         },
         yAxis: {
+          name: 'sum_7yr_GP',
           splitLine: {
             lineStyle: {
               type: 'dashed'
@@ -90,6 +106,7 @@ export default {
       })
     },
     updateGraph: function() {
+      let self = this
       let start = this.startYear.getFullYear()
       let end = this.endYear.getFullYear()
       let columnIndex = this.labels.indexOf(this.selectedColumn)
@@ -97,6 +114,9 @@ export default {
       let cleanData = this.allData.filter(item => {
         return parseInt(item[INDEX_YEAR]) >= start && parseInt(item[INDEX_YEAR]) <= end
       })
+      if (self.leafNode !== -1) {
+        cleanData = cleanData.filter(item => parseInt(item[INDEX_LEAF]) === self.leafNode)
+      }
       cleanData = cleanData.map(item => [item[columnIndex], item[INDEX_Y], item[INDEX_NAME]])
       this.drawPlot(cleanData)
     }
